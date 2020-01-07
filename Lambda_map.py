a84e498ec6fc4704844894c4d64717aec6935d7f
AnastasiiaDm <37839136+AnastasiiaDm@users.noreply.github.com>
2020-01-07 18:14:57 +0200

    return added

--- Lambda_map.py
+++ Lambda_map.py
@@ -5,23 +5,9 @@ book_list = [
     [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]
 ]
 
-list(zip(list(map(lambda y: y[0], book_list)), (list(map(lambda x: x[2] * (x[3] + 10) if x[2] * x[3] < 100 else x[2] * x[3], book_list)))))
+
+def zada4a(book_list):
+    return list(zip(list(map(lambda y: y[0], book_list)), (list(map(lambda x: x[2] * (x[3] + 10) if x[2] * x[3] < 100 else x[2] * x[3], book_list)))))
 
 
-
-# def book_tuple(book_list):
-#     for x in book_list:
-#         summ = x[2] * x[3]
-#         if summ < 100:
-#             result_plus10 = x[0], x[2] * (x[3] + 10)
-#             print(result_plus10)
-#         else:
-#             result = x[0], summ
-#             print(result)
-#
-#
-# book_tuple(book_list)
-
-
-
-
+zada4a(book_list)
